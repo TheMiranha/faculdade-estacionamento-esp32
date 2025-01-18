da55d7bf2c032d04c35d0f3e1c32b8fd577360cf
Lucas Miranda <53923502+TheMiranha@users.noreply.github.com>
2025-01-17 21:50:19 -0300

    Update micro.ino

--- micro.ino
+++ micro.ino
@@ -1,136 +1,137 @@
 #include <WiFi.h>
-#include <HTTPClient.h>
-#include <Arduino_JSON.h>
+#include <String.h>
+#include <PubSubClient.h>
 
-#define TR1 18
-#define EC1 19
-#define R1 32
-#define G1 33
-#define Y1 2
+// LEDS
+#define R 32
+#define G 33
+#define Y 2
 
-const char* ssid = "Lucas's Galaxy S22";
-const char* password = "slapo310";
+// SENSORS
+#define TR 18
+#define EC 19
 
-String url = "http://192.168.91.165:25565";
+const char* parkId = "15d98963-f193-4758-9afe-da499a324490";
+const char* buyTopic = "/park/buy/15d98963-f193-4758-9afe-da499a324490";
 
-JSONVar parks;
+const String wifi_ssid = "Fernando";
+const String wifi_password = "fernando190";
 
-void prepareWiFi() {
-  WiFi.begin(ssid, password);
+const char* mqtt_broker = "192.168.15.10";
+const char* mqtt_username = "username";
+const char* mqtt_password = "password";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+bool isOwned = false;
+bool hasObject = false;
+
+
+void prepareWifi(){
+  WiFi.begin(wifi_ssid, wifi_password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.print("Conectado ao WiFi: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("[WiFi]: connected: "+ wifi_ssid);
 }
 
-void getParks() {
-    HTTPClient http;
-    String serverPath = url + "/parks";
+void prepareMqtt() {
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(mqtt_callback);
 
-    http.begin(serverPath.c_str());
-
-    http.addHeader("Authorization", "Bearer IUSHEFD9PHp9h09phusadf");
-
-    int httpResponseCode = http.GET();
-
-    if (httpResponseCode>0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        String response = http.getString();
-        // Serial.println(response);
-        
-       JSONVar responseObject = JSON.parse(response);
-  
-      // JSON.typeof(jsonVar) can be used to get the type of the var
-      if (JSON.typeof(responseObject) == "undefined") {
-        Serial.println("Parsing input failed!");
-        return;
-      }
-
-      // JSONVar responseObjectKeys = responseObject.keys();
-      JSONVar localParks = responseObject["parks"];
-      for (int parkIndex = 0; parkIndex < localParks.length(); parkIndex++) {
-        JSONVar parkObject = localParks[parkIndex];
-        if (JSON.typeof(parkObject["parking"]) == "null") {
-          parks[parkObject["id"]] = 0;
-        } else {
-          JSONVar parkingObject = parkObject["parking"];
-          parks[parkObject["id"]] = 1;
-        }
-      }
-
+  while (!client.connected()) {
+    String clientId = parkId;
+    if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("[MQTT]: connected");
     } else {
-      Serial.print("Error code: ");
-      Serial.println(httpResponseCode);
+      Serial.println("[MQTT]: error on connect");
+      Serial.println(client.state());
+      delay(2000);
     }
+  }
 
-    http.end();
+  client.subscribe(buyTopic);
 }
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(TR1, OUTPUT);
-  pinMode(EC1, INPUT);
-  
-  pinMode(G1, OUTPUT);
-  pinMode(R1, OUTPUT);
-  pinMode(Y1, OUTPUT);
+  pinMode(R, OUTPUT);
+  pinMode(Y, OUTPUT);
+  pinMode(G, OUTPUT);
 
-  prepareWiFi();
-}
+  pinMode(TR, OUTPUT);
+  pinMode(EC, INPUT);
 
-void showParks() {
-  Serial.println(parks["15d98963-f193-4758-9afe-da499a324490"]);
-  if (JSON.typeof(parks["15d98963-f193-4758-9afe-da499a324490"]) == "null") {
-    Serial.println("Nao pronto");
-    digitalWrite(G1, HIGH);
-    digitalWrite(R1, HIGH);
-    digitalWrite(Y1, HIGH);
-    return;
-  }
-
-  int status = parks["15d98963-f193-4758-9afe-da499a324490"];
-  if (status == 0) {
-
-    digitalWrite(TR1, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TR1, LOW);
-
-    float distance, duration;
-
-    duration = pulseIn(EC1, HIGH);
-
-    distance = 0.017 * duration;
-
-    Serial.print("Distancia: ");
-    Serial.println(distance);
-
-    if (distance <= 2) {
-      digitalWrite(G1, LOW);
-      digitalWrite(R1, LOW);
-      digitalWrite(Y1, HIGH);
-    } else {
-    digitalWrite(G1, HIGH);
-    digitalWrite(R1, LOW);
-    digitalWrite(Y1, LOW);
-    }
-
-    Serial.println("Nao ta reservada");
-  
-  } else {
-    Serial.println("Reservada");
-    digitalWrite(G1, LOW);
-    digitalWrite(R1, HIGH);
-    digitalWrite(Y1, LOW);
-  }
+  prepareWifi();
+  prepareMqtt();
 }
 
 void loop() {
-  getParks();
-  showParks();
-  delay(1500);
+  client.loop();
+  useSensors();
+  useLeds();
+}
+
+void mqtt_callback(char *topic, byte *payload, unsigned int length) {
+  String content = "";
+  for (int i = 0; i < length; i++) {
+    content = content + (char)payload[i];
+  }
+  if (content == "true") {
+    isOwned = true;
+  } else {
+    isOwned = false;
+  }
+}
+
+int sensor_error_calc = 0;
+
+void useSensors() {
+  digitalWrite(TR, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TR, LOW);
+
+  float distance, duration;
+
+  duration = pulseIn(EC, HIGH);
+
+  distance = 0.017 * duration;
+
+  if (distance < 5) {
+    if (sensor_error_calc == 10) {
+      hasObject = true;
+    } else {
+      sensor_error_calc++;
+    }
+  } else {
+    if (sensor_error_calc == -10) {
+      hasObject = false;
+    } else {
+      sensor_error_calc--;
+    }
+  }
+}
+
+void useLeds() { 
+  if (isOwned) {
+    digitalWrite(R, LOW);
+    digitalWrite(G, LOW);
+    digitalWrite(Y, HIGH);
+    return;
+  }
+
+  if (hasObject) {
+    digitalWrite(R, HIGH);
+    digitalWrite(G, LOW);
+    digitalWrite(Y, LOW);
+  } else {
+    digitalWrite(R, LOW);
+    digitalWrite(G, HIGH);
+    digitalWrite(Y, LOW);
+  }
 }
